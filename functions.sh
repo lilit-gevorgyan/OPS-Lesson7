2497b37384b9ca58e1ebb100a8a43520dda48aa7
aramayis <aramayis.devops@gmail.com>
2022-05-30 21:08:47 +0400

    Add char

--- functions.sh
+++ functions.sh
@@ -6,7 +6,11 @@ if [-d ~/backup];then
 
    echo "dir exists"
 else
+<<<<<<< HEAD
   mkdir -p /backup && echo "dir has been created succesfull"
+=======
+  mkdir -p ~/backup && echo "dir has been created succesfully"
+>>>>>>> e144600450f0f89a836f07b59b6639efa32234f1
 fi
 }
 
